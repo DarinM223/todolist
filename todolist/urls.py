5d35b45619b21c07017b82e54b4043329a10fd81
Darin Minamoto <darinm223@gmail.com>
2014-07-26 00:00:55 -0700

    Added mock user detail page and fixed signup view

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,5 +5,5 @@ from todolist import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signup/', views.UserCreateView.as_view(), name='signup'),
-    url(r'^users/(?P<user_id>\d+)/', views.UserDetailView.as_view(), name='user'),
+    url(r'^users/(?P<username>[-_\w]+)/', views.UserDetailView.as_view(), name='user'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
 from todolist import models
 from todolist import forms
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
@@ -15,8 +16,13 @@ class UserCreateView(CreateView):
     form_class = forms.UserForm
     fields = ['username', 'email', 'password']
     template_name = 'todolist/signup.html'
-    success_url = '/todolist'
+    # redirects to user detail page
+    def get_success_url(self):
+        return reverse('user', args=(self.object.username,))
 
 class UserDetailView(DetailView):
     model = models.User
     template_name = 'todolist/user_detail.html'
+    # gets object from username parameter in url string
+    def get_object(self):
+        return self.get_queryset().get(username=self.kwargs.get("username"))
