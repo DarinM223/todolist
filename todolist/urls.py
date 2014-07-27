45edc0e8e548a06343bda1850e82a6a1d65f8256
Darin Minamoto <darinm223@gmail.com>
2014-07-27 02:20:52 -0700

    Added login/logout functionality

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 
 from todolist import views
+from class_based_auth_views.views import LoginView
+
+# use django-class-based-auth-views for authentication views
+from django.contrib.auth.forms import AuthenticationForm
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', LoginView.as_view(form_class=AuthenticationForm, template_name='todolist/login.html', success_url='/todolist'), name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
     url(r'^signup/', views.UserCreateView.as_view(), name='signup'),
     url(r'^users/(?P<username>[-_\w]+)/', views.UserDetailView.as_view(), name='user'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.edit import CreateView
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView
 from todolist import models
 from todolist import forms
 from django.core.urlresolvers import reverse
 
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'todolist/login.html')
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('user', args=(request.user.username,)))
+    else: 
+        return render(request, 'todolist/index.html')
+
+def logout(request):
+    if request.user.is_authenticated():
+        auth.logout(request)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return HttpResponse('You have to login before you can log out!')
 
 class UserCreateView(CreateView):
     form_class = forms.UserCreateForm
